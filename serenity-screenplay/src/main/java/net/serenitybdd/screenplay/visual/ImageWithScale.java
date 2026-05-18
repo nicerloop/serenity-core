33b0dbb3bfad16227e92b0df457659f0ada82da9
Loïc PÉRON <loic.peron@gmail.com>
2026-05-18 19:00:28 +0200

    add constructors for ImageWithScale

--- serenity-screenplay/src/main/java/net/serenitybdd/screenplay/visual/ImageWithScale.java
+++ serenity-screenplay/src/main/java/net/serenitybdd/screenplay/visual/ImageWithScale.java
@@ -1,9 +1,11 @@
 package net.serenitybdd.screenplay.visual;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import javax.imageio.ImageIO;
+import java.io.UncheckedIOException;
+import java.text.DecimalFormat;
 
 /**
  * A screenshot image with its device pixel ratio scale factor.
@@ -15,19 +17,37 @@ public class ImageWithScale {
 
     private final BufferedImage image;
     private final double scale;
+    private final String description;
+    public static final DecimalFormat scaleFormat = new DecimalFormat("0.##");
 
     public ImageWithScale(byte[] bytes, double scale) {
+        this(readImage(bytes), scale);
+    }
+
+    public static BufferedImage readImage(byte[] bytes) {
         try {
-            this.image = ImageIO.read(new ByteArrayInputStream(bytes));
+            return ImageIO.read(new ByteArrayInputStream(bytes));
         } catch (IOException e) {
-            throw new RuntimeException("Failed to read screenshot image", e);
+            throw new UncheckedIOException("Failed to read screenshot image", e);
         }
-        this.scale = scale;
     }
 
     public ImageWithScale(BufferedImage image, double scale) {
+        this(image, scale, makeDescription(image.getWidth(), image.getHeight(), scale));
+    }
+
+    public static String makeDescription(int width, int height, double scale) {
+        StringBuilder desc = new StringBuilder().append(width).append('x').append(height);
+        if (scale != 1.0) {
+            desc.append('@').append(scaleFormat.format(scale));
+        }
+        return desc.toString();
+    }
+
+    public ImageWithScale(BufferedImage image, double scale, String description) {
         this.image = image;
         this.scale = scale;
+        this.description = description;
     }
 
     public BufferedImage getImage() {
@@ -38,11 +58,7 @@ public class ImageWithScale {
         return scale;
     }
 
-    public String description() {
-        String desc = image.getWidth() + "x" + image.getHeight();
-        if (scale != 1.0) {
-            desc += "@" + scale;
-        }
-        return desc;
+    public String getDescription() {
+        return description;
     }
 }
